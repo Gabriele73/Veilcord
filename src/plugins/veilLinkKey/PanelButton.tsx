83447fe2da30dffc5e34d6a423ec9d8d287ae617
gab <gabbitm410@gmail.com>
2026-05-06 00:41:52 +0200

    w

--- src/plugins/veilLinkKey/PanelButton.tsx
+++ src/plugins/veilLinkKey/PanelButton.tsx
@@ -5,7 +5,7 @@
  */
 
 import { cryptoService } from "@plugins/veilCrypto";
-import { Tooltip, useEffect, useState } from "@webpack/common";
+import { useEffect, useState } from "@webpack/common";
 
 import { KeyIcon } from "./KeyIcon";
 
@@ -41,18 +41,16 @@ export function PanelButton({ onClick }: { onClick: () => void; }) {
         : "vc-veil-panel-button vc-veil-needs-key";
 
     return (
-        <Tooltip text={tooltipText} position="top">
-            {tooltipProps => (
-                <button
-                    {...tooltipProps}
-                    type="button"
-                    className={className}
-                    onClick={onClick}
-                    aria-label={ariaLabel}
-                >
-                    <KeyIcon width={18} height={18} />
-                </button>
-            )}
-        </Tooltip>
+        <button
+            type="button"
+            className={className}
+            onClick={onClick}
+            aria-label={ariaLabel}
+        >
+            <KeyIcon width={18} height={18} />
+            <span className="vc-veil-panel-tooltip" role="tooltip">
+                {tooltipText}
+            </span>
+        </button>
     );
 }
